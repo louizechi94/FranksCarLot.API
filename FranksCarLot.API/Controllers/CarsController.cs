a26d22720c1a73c6a0131cdba7a6cf67ff08606b
Terry Brown <31456269+jhondoe256@users.noreply.github.com>
2023-06-16 14:55:59 -0400

    Made Final Changes...HOpeFuLLy?

--- FranksCarLot.API/Controllers/CarsController.cs
+++ FranksCarLot.API/Controllers/CarsController.cs
@@ -2,6 +2,9 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using FranksCarLot.API.FranksCarLot.Modeles.CarModeles;
+using FranksCarLot.Modeles.CarModeles;
+using FranksCarLot.Services.CarServices;
 using Microsoft.AspNetCore.Mvc;
 
 namespace FranksCarLot.API.Controllers
@@ -10,6 +13,68 @@ namespace FranksCarLot.API.Controllers
     [Route("api/[controller]")]
     public class CarsController : ControllerBase
     {
-        
+        private readonly ICarService _carService;
+
+        public CarsController(ICarService carService)
+        {
+            _carService = carService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            return Ok(await _carService.GetCars());
+        }
+
+        [HttpGet("{id:Guid}")]
+        public async Task<IActionResult> Get(Guid id)
+        {
+            var car = await _carService.GetCarById(id);
+            if (car is null)
+                return NotFound();
+            else
+                return Ok(car);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post(CarCreate model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            if (await _carService.AddCar(model))
+                return Ok("Car Created!");
+            else
+                return StatusCode(500, "Internal Server Error.");
+        }
+
+        [HttpPut("{id:Guid}")]
+        public async Task<IActionResult> Put(CarEdit model, Guid id)
+        {
+            if (id != model.Id)
+            {
+                return BadRequest("Invalid Id.");
+            }
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (await _carService.UpdateCar(model))
+                return Ok("Car Updated!");
+            else
+                return StatusCode(500, "Internal Server Error.");
+        }
+
+        [HttpDelete("{id:Guid}")]
+        public async Task<IActionResult> Delete(Guid id)
+        {
+            if (await _carService.DeleteCar(id))
+                return Ok("Car Deleted!");
+            else
+                return StatusCode(500, "Internal Server Error.");
+        }
     }
 }
--- FranksCarLot.API/Controllers/CustomerPurchasesController.cs
+++ FranksCarLot.API/Controllers/CustomerPurchasesController.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using FranksCarLot.Modeles.CustomerPurchaseModeles;
+using FranksCarLot.Services.CustomerPurchaseServices;
 using Microsoft.AspNetCore.Mvc;
 
 namespace FranksCarLot.API.Controllers
@@ -10,6 +12,30 @@ namespace FranksCarLot.API.Controllers
     [Route("api/[controller]")]
     public class CustomerPurchasesController : ControllerBase
     {
-        
+        private readonly ICustomerPurchaseService _cPService;
+
+        public CustomerPurchasesController(ICustomerPurchaseService cPService)
+        {
+            _cPService = cPService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            return Ok(await _cPService.GetCustomerPurchases());
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post(CustomerPurchaseCreate model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            if (await _cPService.AddPurchase(model))
+                return Ok("Customer Purchased a Car!");
+            else
+                return StatusCode(500, "Internal Server Error.");
+        }
     }
 }
